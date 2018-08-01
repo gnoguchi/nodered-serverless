1eebee55ccab4a31b39bed2c071ca05b8d37ff58
gabriel carmelo noguchi <gabriel1tdsc@gmail.com>
2018-08-01 14:15:51 -0300

    fix config credentials error

--- lambda.js
+++ lambda.js
@@ -5,13 +5,13 @@ const express = require('express');
 const app = express();
 const settings = require('./settings')
 const binaryMimeTypes = [
-	'application/octet-stream',
-	'font/eot',
-	'font/opentype',
-	'font/otf',
-	'image/jpeg',
-	'image/png',
-	'image/svg+xml'
+  'application/octet-stream',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml'
 ]
 
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
@@ -21,6 +21,8 @@ var init = (() => {
   // app.use(settings.httpAdminRoot,RED.httpAdmin);
   app.use(settings.httpNodeRoot, RED.httpNode);
   return RED.start().then(() => {
+    // const globalFlow = RED.nodes.getNode(['http in']);
+    // console.log(globalFlow);
     console.log('Node-RED server started.')
   })
 
@@ -28,10 +30,19 @@ var init = (() => {
 
 
 
-exports.handler = (event, context) => {
+exports.handler = (event, context, callback) => {
   init.then(() => {
     RED.nodes.loadFlows().then(() => {
-      awsServerlessExpress.proxy(server, event, context)
+      // awsServerlessExpress.proxy(server, event, context)
+
+      // const globalFlow = RED.nodes.getFlow('global');
+      const globalFlow = RED.nodes.getNode('a8a21d26.83b6d');
+      console.log(globalFlow);
+
+      //return callback(null, globalFlow);
+       callback(null, "success");
+
     })
+
   })
 }
