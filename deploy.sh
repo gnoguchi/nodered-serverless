6e8b3dea87e3b5811c76ee56855f600cd198f1d9
gabriel carmelo noguchi <gabriel1tdsc@gmail.com>
2018-08-11 22:02:10 -0300

    get s3 flows for running node red

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 echo -e "########################################"
-read -e -p "Type 'c' - Create or 'u' - Update lambda function. [c] ou [u]:" choice
+read -n1 -e -p "Type 'c' - Create, 'u' - Update or 'd' to Destroy lambda function. [c] or [u] or [d]:" choice
 echo -e "########################################"
 
 
 
 if [ "$choice" == "c" ]; then
-    claudia create --handler lambda.handler --set-env HOME=/tmp  --deploy-proxy-api --region us-west-2 
+    claudia create --handler lambda.handler --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME --deploy-proxy-api --region us-west-2 
 elif [ "$choice" == "u" ]; then
-    claudia update --handler lambda.handler --set-env HOME=/tmp --deploy-proxy-api --region us-west-2
+    claudia update --handler lambda.handler --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME  --deploy-proxy-api --region us-west-2
+elif [ "$choice" == "d" ]; then
+    claudia destroy --handler lambda.handler --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME  --deploy-proxy-api --region us-west-2
 else
     echo "Error: invalid option!"
 fi
--- lambda.js
+++ lambda.js
@@ -1,23 +1,73 @@
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express')
-const RED = require('node-red');
-const express = require('express');
-const app = express();
-const settings = require('./settings')
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs');
+const AWS = require('aws-sdk');
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
+const s3 = new AWS.S3();
+//const cors = require('cors')
+//const compression = require('compression')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const RED = require('node-red')
 const when = require('when');
-const http = require('http');
+//const settings = require('./settings')
+
+
+const app = express()
+
+var settings = {
+  disableEditor: true,
+  httpAdminRoot: false,
+  httpNodeRoot: '/',
+  httpStatic: 'public',
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false
+    }
+  },
+  //awsRegion: process.env.AWS_REGION,
+  //awsS3Bucket: process.env.S3_BUCKET,
+  //awsS3Appname: process.env.AWS_LAMBDA_FUNCTION_NAME,
+  //storageModule: require('node-red-contrib-storage-s3'),
+  functionGlobalContext: {},
+  credentialSecret: process.env.NODE_RED_SECRET || "a-secret-key",
+  flowFile: '/tmp/' + process.env.FLOW_NAME
+  
+};
+
 const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
   'application/octet-stream',
+  'application/xml',
   'font/eot',
   'font/opentype',
   'font/otf',
   'image/jpeg',
   'image/png',
-  'image/svg+xml'
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
 ]
 
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
+//app.use(compression())
+//app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(awsServerlessExpressMiddleware.eventContext())
 
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
 var delay = (msec) => {
   return when.promise((resolve) => {
@@ -27,37 +77,28 @@ var delay = (msec) => {
   })
 }
 
-
-
 const init = (() => {
-  RED.init(server, settings)
-  // app.use(settings.httpAdminRoot,RED.httpAdmin);
-  app.use(settings.httpNodeRoot, RED.httpNode);
-  return RED.start().then(() => {
+  const file = fs.createWriteStream('/tmp/' + process.env.FLOW_NAME);
+  s3.getObject({
+    Bucket: 'nodered-lambda-flows',
+    Key: process.env.FLOW_NAME
+  }).createReadStream().pipe(file);
 
+
+  RED.init(server, settings);
+  app.use(settings.httpNodeRoot, RED.httpNode);
+  
+  return RED.start().then(() => {
     console.log('Node-RED server started.')
     return delay(1000)
-
   })
 
 })()
 
-
-
-exports.handler = (event, context, callback) => {
+exports.handler = (event, context) => {
   init.then(() => {
     RED.nodes.loadFlows().then(() => {
-
       awsServerlessExpress.proxy(server, event, context)
-
-      // const globalFlow = RED.nodes.getNode(['http in']);
-
-      const globalFlow = RED.nodes.getNode('a8a21d26.83b6d');
-      console.log('BLABLABLA');
-      console.log(globalFlow);
-      return callback(null, globalFlow);
     })
-
   })
-
 }
