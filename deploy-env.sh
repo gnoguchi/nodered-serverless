8b9f631ddef682a868e17a08f3b26e5ef644bd70
gabriel <gabriel1tdsc@gmail.com>
2018-09-27 17:45:28 -0300

    change deploy configs

--- deploy-env.sh
+++ deploy-env.sh
@@ -1,3 +1,4 @@
 export FLOW_NAME="flows_dispatcher.json"
 export CONFIG_FLOW=".config.json"
 export S3_BUCKET="nodered-lambda-flows/dispatcher"
+export LAMBDA_REGION="us-east-1"
--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ echo "SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY"
 echo "S3_BUCKET=$S3_BUCKET"
 
 if [ "$choice" == "c" ]; then
-    claudia create --handler lambda.handler --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME,ACCESS_KEY_ID=$ACCESS_KEY_ID,SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY,CONFIG_FLOW=$CONFIG_FLOW,S3_BUCKET=$S3_BUCKET --memory 1280 --timeout 10  --deploy-proxy-api --region us-west-2 
+    claudia create --config nodered-lambda.json --runtime nodejs8.10 --region $LAMBDA_REGION --deploy-proxy-api --handler lambda.handler --version dev --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME,CLAUDIA_AWS_ACCESS_KEY_ID=$CLAUDIA_AWS_ACCESS_KEY_ID,CLAUDIA_AWS_SECRET_ACCESS_KEY=$CLAUDIA_AWS_SECRET_ACCESS_KEY,CONFIG_FLOW=$CONFIG_FLOW,S3_BUCKET=$S3_BUCKET --memory 3008 --timeout 10 --role arn:aws:iam::849805908400:role/nodered-lambda --name nodered-lambda
 elif [ "$choice" == "u" ]; then
-    claudia update --handler lambda.handler --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME,ACCESS_KEY_ID=$ACCESS_KEY_ID,SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY,CONFIG_FLOW=$CONFIG_FLOW,S3_BUCKET=$S3_BUCKET --memory 1280 --timeout 10  --deploy-proxy-api --region us-west-2
+    claudia update --config nodered-lambda.json --runtime nodejs8.10 --region $LAMBDA_REGION --deploy-proxy-api --handler lambda.handler --version dev --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME,CLAUDIA_AWS_ACCESS_KEY_ID=$CLAUDIA_AWS_ACCESS_KEY_ID,CLAUDIA_AWS_SECRET_ACCESS_KEY=$CLAUDIA_AWS_SECRET_ACCESS_KEY,CONFIG_FLOW=$CONFIG_FLOW,S3_BUCKET=$S3_BUCKET --memory 3008 --timeout 10 --role arn:aws:iam::849805908400:role/nodered-lambda --name nodered-lambda
 elif [ "$choice" == "d" ]; then
-    claudia destroy --handler lambda.handler --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME,ACCESS_KEY_ID=$ACCESS_KEY_ID,SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY,CONFIG_FLOW=$CONFIG_FLOW,S3_BUCKET=$S3_BUCKET --memory 1280 --timeout 10  --deploy-proxy-api --region us-west-2
+    claudia destroy --config nodered-lambda.json --runtime nodejs8.10 --region $LAMBDA_REGION --deploy-proxy-api --handler lambda.handler --version dev --profile default --set-env HOME=/tmp,FLOW_NAME=$FLOW_NAME,CLAUDIA_AWS_ACCESS_KEY_ID=$CLAUDIA_AWS_ACCESS_KEY_ID,CLAUDIA_AWS_SECRET_ACCESS_KEY=$CLAUDIA_AWS_SECRET_ACCESS_KEY,CONFIG_FLOW=$CONFIG_FLOW,S3_BUCKET=$S3_BUCKET --memory 3008 --timeout 10 --role arn:aws:iam::849805908400:role/nodered-lambda-executor --name nodered-lambda  
 else
     echo "Error: invalid option!"
 fi
